412bf5710ac2a54c8beff827911bae42e4fe148c
Josh Sawyer <sawyerj6@msu.edu>
2014-04-20 20:38:42 -0400

    added jquery nave bar and a bit of other positioning

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,33 @@
-//show alternate image on mouse rollover
-$(function () {
-    $('#stars img').each(function () {
-        $(this).data('original', this.src)
-    }).mouseenter(function () {
-        $(this)
-            .attr('src', $(this).data('hover'))
-            .animate({})
-    }).mouseleave(function () {
-        $(this)
-            .attr('src', $(this).data('original'))
-        .animate({})
-    })
-})
+$(document).ready(function(){
 
-//hide the text div and show when hovering over the image
-$("#stars div").hide();
-$('#stars img').hover(
-  function () {
-    $(this).next("#stars div").slideToggle();
-  });
+    //show alternate image on mouse rollover
+    $(function () {
+        $('#stars img').each(function () {
+            $(this).data('original', this.src)
+        }).mouseenter(function () {
+            $(this)
+                .attr('src', $(this).data('hover'))
+                .animate({})
+        }).mouseleave(function () {
+            $(this)
+                .attr('src', $(this).data('original'))
+            .animate({})
+        });
+    });
+
+    //hide the text div and show when hovering over the image
+    $("#stars div").hide();
+    $('#stars img').hover(
+      function () {
+        $(this).next("#stars div").slideToggle();
+      });
+
+    //jquery nav bounceout
+    $(".nav li").mouseover(function(){
+        $(this).stop().animate({height:'50px'})
+    });
+    $(".nav li").mouseout(function(){
+        $(this).stop().animate({height:'25px'})
+    });
+
+});
